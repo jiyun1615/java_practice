800c01eb4e61d1e5f0e4cfe3d82434661925213a
JIYUN <yunn1999@naver.com>
2020-10-12 14:19:43 +0900

    practice 상속

---
+++ class_practice/src/class_practice/Shape.java
@@ -0,0 +1,18 @@
+package class_practice;
+
+public class Shape {
+	private int x;
+	private int y;
+	
+	public int getX() {	return x;}
+	public void setX(int x) {	this.x = x;}
+	public int getY() {	return y;}
+	public void setY(int y) {	this.y = y;}
+	
+	void print()
+	{
+		System.out.println("xÁÂÇ¥: " + x + " yÁÂÇ¥: " + y);
+	}
+
+
+}
