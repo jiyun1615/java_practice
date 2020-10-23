58ba38dff9ad44f7a37eb2e43d5dbf463e260f4b
JIYUN <yunn1999@naver.com>
2020-10-23 16:44:09 +0900

    report5 update 1023

--- report5/src/report5/Cat.java
+++ report5/src/report5/Cat.java
@@ -7,7 +7,7 @@ public class Cat extends Animal implements Happy{
 		age = "3살";
 	}
 	void introduce() {
-		System.out.println("종: "+ species + " 이름: " + name + " 나이 : " + age);
+		System.out.println("종: "+ species + "  이름: " + name + "  나이 : " + age);
 		System.out.println("저의 애완묘 " + name + "!");
 	}
 	public String play() {
--- report5/src/report5/report5.java
+++ report5/src/report5/report5.java
@@ -8,17 +8,20 @@ public class report5 {
 		System.out.print("나의 애완동물 : 1.개 2.고양이 3.햄스터 (선택)");
 		int num = stdin.nextInt();
 		
+		Animal myAnimal = null;
+		Happy myAnimal1 = null;
+		
 		if (num == 1) {
-			Animal myAnimal = new Dog();
-			Happy myAnimal1 = new Dog();
+			myAnimal = new Dog();
+			myAnimal1 = new Dog();
 		}
 		else if (num == 2) {
-			Animal myAnimal = new Cat();
-			Happy myAnimal1 = new Cat();
+			myAnimal = new Cat();
+			myAnimal1 = new Cat();
 		}
 		else if (num == 3) {
-			Animal myAnimal = new Hamster();
-			Happy myAnimal1 = new Hamster();
+			myAnimal = new Hamster();
+			myAnimal1 = new Hamster();
 		}
 		else 
 			System.out.println("숫자를 잘못 입력하셨습니다.");
