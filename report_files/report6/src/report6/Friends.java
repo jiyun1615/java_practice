f741f7c09405900138d785227727ed01af943b6e
JIYUN <yunn1999@naver.com>
2020-11-07 15:48:41 +0900

    Friends class (Serialize)

---
+++ report_files/report6/src/report6/Friends.java
@@ -0,0 +1,14 @@
+package report6;
+import java.io.Serializable;
+
+public class Friends implements Serializable{
+	public String name;
+	public String birth;
+	public String phoneNumber;	
+	
+	//이해안감 override
+	public String toString() {
+		return ("Name : " + name + "\t Birth : " + birth + "\t Phone number : " + phoneNumber);
+	}
+	
+}
